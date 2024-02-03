98abcf17f7df6da3b7e329442abe71c8884d3ae6
[Eitel Tchapdeu] <[eiteltchapdeu@gmail.com>
2024-02-03 19:34:02 +0000

    Added the Welcome modal pages in HTML and input capture in JS with some CSS

--- Assets/script.js
+++ Assets/script.js
@@ -36,7 +36,12 @@ var GiphyAPI_Key = "JlYM2XmAwmrUDSWWhLTzqgSsAOwV5YZ7"
 var GiphyUrl = "api.giphy.com/v1/gifs/translate"
 
 //Global scope variable
-var QuoteText = "";
+var QuoteText = ""; 
+var userName = "";
+var sleepGoal = null;
+var caloriesGoal = null;
+var stepsGoal = null;
+
 
 fetch(QuoteUrl, {
   method: 'GET',
@@ -76,6 +81,35 @@ fetch(QuoteUrl, {
 $(function() {
     $("#calendar-box").datepicker();
   } );
+
+//Modal page
+//Page 1 - Capture username input in variables
+$('#save-name').on("click", function(event) {
+    event.preventDefault();
+    userName = $('#username').val();
+    console.log("Username: " + userName);
+});
+
+//Page 2 - Capture sleep goal inputs (total in minutes) in variables 
+$('#save-sleep').on("click", function(event) {
+    event.preventDefault();
+    sleepGoal = parseInt(($('#sleep-goal-hour').val()*60), 10) + parseInt($('#sleep-goal-minute').val(), 10);
+    console.log("Sleep Goal: " + sleepGoal + " minutes");
+});
+
+//Page 3 - Capture nutrition goal inputs in variables
+$('#save-calories').on("click", function(event) {
+    event.preventDefault();
+    caloriesGoal = parseInt($('#calories-goal').val(), 10);
+    console.log("Calories Goal: " + caloriesGoal + " calories");
+});
+
+//Page 4 - Capture steps goal inputs in variables
+$('#save-steps').on("click", function(event) {
+    event.preventDefault();
+    stepsGoal = parseInt($('#steps-goal').val(), 10);
+    console.log("Steps Goal: " + stepsGoal + " steps");
+});
 
 
 
