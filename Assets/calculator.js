7a7cc3d9db420bb3571dba9b39b074173ccad210
[Eitel Tchapdeu] <[eiteltchapdeu@gmail.com>
2024-02-06 19:49:45 +0000

    Added calculator.js and changed journal.html a bit

---
+++ Assets/calculator.js
@@ -0,0 +1,131 @@
+//Edamam API fetch:
+//Edamam API ID and keys
+var EdamanAPI_ID = "181983d3";
+var EdamanAPI_Key = "f419ea241fd50c7fbf94fe990f801e76";
+
+//Global scope variable
+var scoreData = [];
+
+//Today's date
+var todaysDate = dayjs().format("dddd, DD MMMM YYYY");
+$("#todays-date").text(todaysDate); //Use this tag
+
+//Score calcs - inputs from HTML page: journal.html
+//variables to capture from journal input apply parseInt(x,10)
+var sleepInput = ""; //Input for daily sleep amount (minutes)
+var caloriesInput = 0; //Input for daily calories (cal) intake taken from API calcs
+var stepsInput = ""; //Input for daily steps (steps)
+
+//Event listener when inputs are submitted
+$('#calories-btn').on("click", function() { 
+    //Calories input calcs based on text inputs - Edaman API fetch
+    
+    var food_input = $('#nutrition-input').val();  //Use this tag
+    console.log("food input is: " + food_input)
+
+    // Construct the URL based on text provided
+    var EdQueryUrl = "https://api.edamam.com/api/nutrition-data?app_id=" + EdamanAPI_ID + "&app_key=" + EdamanAPI_Key + "&ingr=" + food_input;
+    
+    //Fetch call to get calory intake
+    fetch(EdQueryUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data1) {
+            //Capture calory count
+            console.log(data1);
+            caloriesInput = data1.calories;
+            console.log("Total calorie: " + caloriesInput + " cal");
+            localStorage.setItem('calories',JSON.stringify(caloriesInput));
+
+            //Show in the page the equivalent calory count
+            $('#calories-result').text("Based on your input you have consumed " + caloriesInput + " calories on this day!") //Use this tag
+        });
+
+        return caloriesInput;
+});
+
+$('#submit-input-btn').on("click", function() {    //Use this tag
+
+    //Sleep input
+    var sleepInput = parseInt(($('#sleep-input-hour').val()*60), 10) + parseInt($('#sleep-input-minute').val(), 10);  //Use this tag
+    //var caloriesInput = JSON.parse(localStorage.getItem('calories'));
+    
+    //Steps input
+    var stepsInput = parseInt($('#steps-input').val(), 10);   //Use this tag
+
+    //Fetch the goals from localstorage
+    var fetchedSleepGoal = JSON.parse(localStorage.getItem('sleepGoal'));
+    var fetchedCaloriesGoal = JSON.parse(localStorage.getItem('caloriesGoal'));
+    var fetchedStepGoal = JSON.parse(localStorage.getItem('stepsGoal'));
+
+    console.log("fetchedSleepGoal: " + fetchedSleepGoal);
+    console.log("fetchedCaloriesGoal: " + fetchedCaloriesGoal);
+    console.log("fetchedStepGoal: " + fetchedStepGoal);
+    console.log("sleepInput: " + sleepInput);
+    console.log("caloriesInput: " + caloriesInput);
+    console.log("stepsInput: " + stepsInput);
+
+    //Sleep score calculation in % sleeping too little or too much is bad
+    if(sleepInput <= fetchedSleepGoal) {
+        sleepScore = parseInt(((sleepInput/fetchedSleepGoal) * 100).toFixed(0),10);
+    } else {
+        sleepScore = parseInt((100 - (((sleepInput - fetchedSleepGoal)/fetchedSleepGoal) * 100)).toFixed(0),10)
+    };
+
+    //Calories score calculation in % eating too little or too much is bad
+    if(caloriesInput <= fetchedCaloriesGoal) {
+        caloriesScore = parseInt(((caloriesInput/fetchedCaloriesGoal) * 100).toFixed(0),10);
+    } else {
+        caloriesScore = parseInt((100 - (((caloriesInput - fetchedCaloriesGoal)/fetchedCaloriesGoal) * 100)).toFixed(0),10)
+    };
+
+    //Steps score calculation in % walking too much is good!
+    if(stepsInput <= fetchedStepGoal) {
+        stepsScore = parseInt(((stepsInput/fetchedStepGoal) * 100).toFixed(0),10);
+    } else {
+        stepsScore = 100;
+    };
+
+    //Average score
+    var averageScore = parseInt(((sleepScore + caloriesScore + stepsScore)/3).toFixed(0),10);
+    console.log(sleepScore);
+    console.log(caloriesScore);
+    console.log(stepsScore);
+
+    console.log(averageScore);
+    //Method 1 - doughnut chart code
+    anychart.onDocumentReady(function() {
+        //Chart data
+        scoreData = [
+            ["sleep-container","sleep",sleepScore],
+            ["Nutrition-container","Nutrition",caloriesScore],
+            ["Steps-container","Steps",stepsScore],
+            ["Average-score-container","Average score",averageScore]
+        ]; 
+
+        //Loop to make 4 charts
+        for(let j = 0; j < scoreData.length; j++) {
+            var eachScore = anychart.data.set([
+                [scoreData[j][1],scoreData[j][2]],
+                ["left",(100-[scoreData[j][2]])]
+            ])
+            if(typeof eachScore === 'undefined') {
+                console.log("Chart function awaits data.")
+            } else {
+            //Pie chart and radius
+            var chart = anychart.pie(eachScore)
+            .innerRadius('50%');
+            //Chart title and container id
+            chart.title(scoreData[j][1] + " = " + scoreData[j][2] + "%");
+            let containerName = scoreData[j][0];
+            console.log(containerName);
+            chart.container(containerName);
+            chart.draw();
+            }
+        };
+    })
+    
+    
+});
+
--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,4 @@
 //API fetches:
-// Edamam API fetch:
-//Edamam API keys
-var EdamanAPI_ID = "181983d3";
-var EdamanAPI_Key = "f419ea241fd50c7fbf94fe990f801e76";
-
- // Food input example
-var food_input = "100g of sweetcorn and 50g of biscuit and 20g of rice";
-
- // Construct the URL
- var EdQueryUrl = "https://api.edamam.com/api/nutrition-data?app_id=" + EdamanAPI_ID + "&app_key=" + EdamanAPI_Key + "&ingr=" + food_input;
- 
-//Fetch call to get calory intake
-fetch(EdQueryUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data1) {
-        //Capture calory count
-        console.log(data1);
-        var total_calories = data1.calories;
-        console.log("Total calorie: " + total_calories + " cal");
-    });
-
 //Daily Motivational Quote API fetch
 var QuoteUrl = 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info';
 
@@ -36,46 +13,6 @@ var userName = "";
 var sleepGoal = null;
 var caloriesGoal = null;
 var stepsGoal = null;
-
-
-fetch(QuoteUrl, {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '6c2681c562msh9973791c7ac2a05p14b0b6jsn4b5033f34072',
-    'X-RapidAPI-Host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com'
-  }
-})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data2) {
-        //Return Quote
-        console.log(data2);
-        QuoteText = data2.text;
-        var QuoteAuthor = data2.author;
-        console.log("Quote: " + QuoteText);
-        console.log("Author: " + QuoteAuthor);
-
-        //Fetch from Giphy the best gif based on today's quote using endPoint and API 
-        var GiphyQueryUrl = "https://api.giphy.com/v1/gifs/translate?&api_key=JlYM2XmAwmrUDSWWhLTzqgSsAOwV5YZ7&s=God always takes the simplest way.";
-        var GiphyQueryUrl = "https://api.giphy.com/v1/gifs/translate?&api_key=JlYM2XmAwmrUDSWWhLTzqgSsAOwV5YZ7&s=" + QuoteText;
-        fetch(GiphyQueryUrl)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(data3) {
-                console.log(data3);
-                var GifUrl = data3.data.images.original.url;
-                console.log(GifUrl);
-            })
-    });
-
-// Next for the homepage, make calendar
-// id of container is "calendar-box"
-//jQuery UI for datepicker and dynamically insert calendar in index.html
-$(function() {
-    $("#calendar-box").datepicker();
-  } );
 
 //Modal page
 //Page 1 - Capture username input in variables
@@ -114,44 +51,11 @@ $('#save-steps').on("click", function(event) {
     console.log("Steps Goal: " + stepsGoal + " steps");
 });
 
+// Next for the homepage, make calendar
+// id of container is "calendar-box"
+//jQuery UI for datepicker and dynamically insert calendar in index.html
+$(function() {
+    $("#calendar-box").datepicker();
+  } );
 
-
-//JavaScript for Journal
-var todaysDate = dayjs().format("dddd, DD MMMM YYYY");
-$("#todays-date").text(todaysDate);
-
-
-//Score calcs - inputs from HTML page: journal.html
-//variables to capture from journal input apply parseInt(x,10)
-var sleepInput = ""; //Input for daily sleep amount (minutes)
-var caloriesInput = parseInt(total_calories,10); //Input for daily calories (cal) intake taken from API calcs
-var stepsInput = ""; //Input for daily steps (steps)
-
-// sleepGoal in minutes
-// caloriesGoal in calories
-// stepsGoal in steps
-
-var sleepScore = sleepInput/sleepGoal * 100; //sleep score in %
-var caloriesScore = caloriesInput/caloriesGoal * 100; //nutrition score in %
-var stepsScore = stepsInput/stepsGoal * 100; //steps goals in %
-
-//Sleep score calculation in % sleeping too little or too much is bad
-if(sleepInput <= sleepGoal) {
-    sleepScore = ((sleepInput/sleepGoal, 10) * 100).toFixed(0);
-} else {
-    sleepScore = (100 - (((sleepInput - sleepGoal)/sleepGoal) * 100)).toFixed(0)
-};
-
-//calories score calculation in % eating too little or too much is bad
-if(caloriesInput <= caloriesGoal) {
-    caloriesScore = ((caloriesInput/caloriesGoal, 10) * 100).toFixed(0);
-} else {
-    caloriesScore = (100 - (((caloriesInput - caloriesGoal)/caloriesGoal) * 100)).toFixed(0)
-};
-
-//steps score calculation in % eating too much is good!
-if(stepsInput <= stepsGoal) {
-    stepsScore = ((stepsInput/stepsGoal, 10) * 100).toFixed(0);
-} else {
-    stepsScore = 100;
-};
+//Calendar functionality
