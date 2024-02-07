03beafaa5a61fb7e54c300dc777f311f5ec204dc
[Eitel Tchapdeu] <[eiteltchapdeu@gmail.com>
2024-02-07 21:55:06 +0000

    Updated the JavaScripts and style

--- Assets/calculator.js
+++ Assets/calculator.js
@@ -5,6 +5,23 @@ var EdamanAPI_Key = "f419ea241fd50c7fbf94fe990f801e76";
 
 //Global scope variable
 var scoreData = [];
+var wrongEntry1 = "Please enter your daily food intake properly :), e.g. 250g of chicken breast and 0.5l of orange juice";
+var wrongEntry2 ="Please, enter your data correctly :)";
+
+function showErrorMessage(id,message) {
+    //Add pop up that says to enter entries correctly
+    console.log("Error Message");
+    $(id).removeClass();
+    $(id).addClass("start");
+    $(id).html(message);
+    console.log($(id));
+    console.log(message);
+    //Only display feedback for 5 second
+    setTimeout(function(){
+        $(id).removeClass();
+        $(id).addClass("hide"); //Hide feedback
+    }, 5000); //Show feedback for 5 second.
+}
 
 //Today's date
 var todaysDate = dayjs().format("dddd, DD MMMM YYYY");
@@ -18,32 +35,46 @@ var stepsInput = ""; //Input for daily steps (steps)
 
 //Event listener when inputs are submitted
 $('#calories-btn').on("click", function() { 
-    //Calories input calcs based on text inputs - Edaman API fetch
+    //If statement if entries are not done correctly
     
-    var food_input = $('#nutrition-input').val();  //Use this tag
-    console.log("food input is: " + food_input)
 
-    // Construct the URL based on text provided
-    var EdQueryUrl = "https://api.edamam.com/api/nutrition-data?app_id=" + EdamanAPI_ID + "&app_key=" + EdamanAPI_Key + "&ingr=" + food_input;
+    console.log("Working");
+    console.log(typeof(sleepInput));
+    console.log(typeof(caloriesInput));
+    console.log(typeof(stepsInput));
     
-    //Fetch call to get calory intake
-    fetch(EdQueryUrl)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data1) {
-            //Capture calory count
-            console.log(data1);
-            caloriesInput = data1.calories;
-            console.log("Total calorie: " + caloriesInput + " cal");
-            localStorage.setItem('calories',JSON.stringify(caloriesInput));
+        //Calories input calcs based on text inputs - Edaman API fetch
+        
+        var food_input = $('#nutrition-input').val();  //Use this tag
+        console.log("food input is: " + food_input)
 
-            //Show in the page the equivalent calory count
-            $('#calories-result').text("Based on your input you have consumed " + caloriesInput + " calories on this day!") //Use this tag
-        });
-
+        if(isNaN(food_input)) {
+        // Construct the URL based on text provided
+        var EdQueryUrl = "https://api.edamam.com/api/nutrition-data?app_id=" + EdamanAPI_ID + "&app_key=" + EdamanAPI_Key + "&ingr=" + food_input;
+        
+        //Fetch call to get calory intake
+        fetch(EdQueryUrl)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data1) {
+                //Capture calory count
+                console.log(data1);
+                if(data1.calories === 0) {
+                    //return a try again message
+                caloriesInput = data1.calories;
+                console.log("Total calorie: " + caloriesInput + " cal");
+                localStorage.setItem('calories',JSON.stringify(caloriesInput));
+                }
+                //Show in the page the equivalent calory count
+                $('#calories-result').text("Based on your input you have consumed " + caloriesInput + " calories on this day!") //Use this tag
+            });
+            console.log("Calculate");
         return caloriesInput;
-});
+        } else {
+            showErrorMessage('#wrong-entry1',wrongEntry1);
+        }
+});    
 
 $('#submit-input-btn').on("click", function() {
 
@@ -54,76 +85,94 @@ $('#submit-input-btn').on("click", function() {
     //Steps input
     var stepsInput = parseInt($('#steps-input').val(), 10);   
 
-    //Fetch the goals from localstorage
-    var fetchedSleepGoal = JSON.parse(localStorage.getItem('sleepGoal'));
-    var fetchedCaloriesGoal = JSON.parse(localStorage.getItem('caloriesGoal'));
-    var fetchedStepGoal = JSON.parse(localStorage.getItem('stepsGoal'));
-
-    console.log("fetchedSleepGoal: " + fetchedSleepGoal);
-    console.log("fetchedCaloriesGoal: " + fetchedCaloriesGoal);
-    console.log("fetchedStepGoal: " + fetchedStepGoal);
-    console.log("sleepInput: " + sleepInput);
-    console.log("caloriesInput: " + caloriesInput);
-    console.log("stepsInput: " + stepsInput);
-
-    //Sleep score calculation in % sleeping too little or too much is bad
-    if(sleepInput <= fetchedSleepGoal) {
-        sleepScore = parseInt(((sleepInput/fetchedSleepGoal) * 100).toFixed(0),10);
+    //If statment if entries are not done correctly
+    if(isNaN(sleepInput) || caloriesInput === 0 || isNaN(stepsInput)) {
+        showErrorMessage('#wrong-entry2',wrongEntry2);
     } else {
-        sleepScore = parseInt((100 - (((sleepInput - fetchedSleepGoal)/fetchedSleepGoal) * 100)).toFixed(0),10)
-    };
-
-    //Calories score calculation in % eating too little or too much is bad
-    if(caloriesInput <= fetchedCaloriesGoal) {
-        caloriesScore = parseInt(((caloriesInput/fetchedCaloriesGoal) * 100).toFixed(0),10);
-    } else {
-        caloriesScore = parseInt((100 - (((caloriesInput - fetchedCaloriesGoal)/fetchedCaloriesGoal) * 100)).toFixed(0),10)
-    };
-
-    //Steps score calculation in % walking too much is good!
-    if(stepsInput <= fetchedStepGoal) {
-        stepsScore = parseInt(((stepsInput/fetchedStepGoal) * 100).toFixed(0),10);
-    } else {
-        stepsScore = 100;
-    };
-
-    //Average score
-    var averageScore = parseInt(((sleepScore + caloriesScore + stepsScore)/3).toFixed(0),10);
-    console.log(sleepScore);
-    console.log(caloriesScore);
-    console.log(stepsScore);
-
-    console.log(averageScore);
-    //Method 1 - doughnut chart code
-    anychart.onDocumentReady(function () {  
-        //Chart data
-        scoreData = [
-            ["sleep-container","sleep",sleepScore],
-            ["Nutrition-container","Nutrition",caloriesScore],
-            ["Steps-container","Steps",stepsScore],
-            ["Average-score-container","Average score",averageScore]
-        ]; 
-
-        //Loop to make 4 charts
-        for(let j = 0; j < scoreData.length; j++) {
-            var eachScore = anychart.data.set([
-                [scoreData[j][1],scoreData[j][2]],
-                ["left",(100-[scoreData[j][2]])]
-            ])
-            if(typeof eachScore === 'undefined') {
-                console.log("Chart function awaits data.")
-            } else {
-            //Pie chart and radius
-            var chart = anychart.pie(eachScore)
-            .innerRadius('50%');
-            //Chart title and container id
-            chart.title(scoreData[j][1] + " = " + scoreData[j][2] + "%");
-            let containerName = scoreData[j][0];
-            console.log(containerName);
-            chart.container(containerName);
-            chart.draw();
-            }
-        }
-    })
+        //Run the function
+        RunAndShow(sleepInput,caloriesInput,stepsInput)
+    }
 });
 
+
+function RunAndShow(sleepInput,caloriesInput,stepsInput){
+        //Fetch the goals from localstorage
+        var fetchedSleepGoal = JSON.parse(localStorage.getItem('sleepGoal'));
+        var fetchedCaloriesGoal = JSON.parse(localStorage.getItem('caloriesGoal'));
+        var fetchedStepGoal = JSON.parse(localStorage.getItem('stepsGoal'));
+
+        console.log("fetchedSleepGoal: " + fetchedSleepGoal);
+        console.log("fetchedCaloriesGoal: " + fetchedCaloriesGoal);
+        console.log("fetchedStepGoal: " + fetchedStepGoal);
+        console.log("sleepInput: " + sleepInput);
+        console.log("caloriesInput: " + caloriesInput);
+        console.log("stepsInput: " + stepsInput);
+
+        //Sleep score calculation in % sleepting too little or too much is bad
+        if(sleepInput <= fetchedSleepGoal) {
+            sleepScore = parseInt(((sleepInput/fetchedSleepGoal) * 100).toFixed(0),10);
+        } else if(sleepInput > (2 * fetchedSleepGoal)) {
+            sleepScore = 0;     //If input is unrealistic to avoid negative score.
+        } else {
+            sleepScore = parseInt((100 - (((sleepInput - fetchedSleepGoal)/fetchedSleepGoal) * 100)).toFixed(0),10)
+        };
+
+        //Calories score calculation in % eating too little or too much is bad
+        if(caloriesInput <= fetchedCaloriesGoal) {
+            caloriesScore = parseInt(((caloriesInput/fetchedCaloriesGoal) * 100).toFixed(0),10);
+        } else if(caloriesInput > (2 * fetchedCaloriesGoal)) {
+            sleepScore = 0;     //If input is unrealistic to avoid negative score.
+        } else {
+            caloriesScore = parseInt((100 - (((caloriesInput - fetchedCaloriesGoal)/fetchedCaloriesGoal) * 100)).toFixed(0),10)
+        };
+
+        //Steps score calculation in % walking too much is good!
+        if(stepsInput <= fetchedStepGoal) {
+            stepsScore = parseInt(((stepsInput/fetchedStepGoal) * 100).toFixed(0),10);
+        } else if(isNaN(stepsInput) || stepsInput === 0){
+            stepsScore = 0;
+        } else {
+            stepsScore = 100;
+        };
+
+        //Average score
+        var averageScore = parseInt(((sleepScore + caloriesScore + stepsScore)/3).toFixed(0),10);
+        console.log("sleep score: " + sleepScore);
+        console.log("calories score: " + caloriesScore);
+        console.log("step score: " + stepsScore);
+
+        console.log(averageScore);
+        //Method 1 - doughnut chart code
+        anychart.onDocumentReady(function () {  
+            //Chart data
+            scoreData = [
+                ["sleep-container","sleep",sleepScore],
+                ["Nutrition-container","Nutrition",caloriesScore],
+                ["Steps-container","Steps",stepsScore],
+                ["Average-score-container","Average score",averageScore]
+            ]; 
+            //Save dataset to local Storage
+            localStorage.setItem(JSON.stringify(todaysDate),JSON.stringify(scoreData));
+            
+            //Loop to make the charts
+            for(let j = 0; j < scoreData.length; j++) {
+                var eachScore = anychart.data.set([
+                    [scoreData[j][1],scoreData[j][2]],
+                    ["left",(100-[scoreData[j][2]])]
+                ])
+                if(typeof eachScore === 'undefined') {
+                    console.log("Chart function awaits data.")
+                } else {
+                //Pie chart and radius
+                var chart = anychart.pie(eachScore)
+                .innerRadius('50%');
+                //Chart title and container id
+                chart.title(scoreData[j][1] + " = " + scoreData[j][2] + "%");
+                let containerName = scoreData[j][0];
+                console.log(containerName);
+                chart.container(containerName);
+                chart.draw();
+                }
+            }
+        })
+};
